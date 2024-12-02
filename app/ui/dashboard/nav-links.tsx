8bc3b5f61bdd5465d01f09cca10297e48021b5c8
Ayahviramempesonah <bayusatriapurnomo@gmail.com>
2024-12-02 10:58:10 +0700

    ok

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -11,7 +11,7 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
-// Map of links to display in the side navigation.
+// Map of links to display in the side navigation. 
 // Depending on the size of the application, this would be stored in a database.
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
@@ -39,6 +39,7 @@ export default function NavLinks() {
                 'bg-sky-100 text-blue-600': pathname === link.href,
               },
             )}
+            
          >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
